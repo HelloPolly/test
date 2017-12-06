01b073bb73d4e77d90e05beff0af061d97da4cfa
HelloPolly <980893561@qq.com>
2017-12-06 16:26:09 +0800

    test

--- requestsDemo.py
+++ requestsDemo.py
@@ -13,14 +13,23 @@ import json
 #print(r.status_code)
 #print(r.headers)
 
-url = "https://postman-echo.com/post" #输入测试的接口地址
-testdata="hello world!"#输入测试输入
-headers={ #输入headers
+#输入测试的接口地址
+url = "https://postman-echo.com/post"
+#输入测试输入
+testdata="hello world!" 
+
+ #输入headers
+headers={
     'content-type':"text/plain"
 }
-r=requests.post(url,data=testdata,headers=headers)#以POSt的方式进行请求，并把返回数据存到r这个变量中
-r=json.loads(r.text)#把r的text的属性内容转换为json格式，并重新赋值给r
-if testdata == r["data"]:#测试数据和返回数据做对比
+#以POSt的方式进行请求，并把返回数据存到r这个变量中
+r=requests.post(url,data=testdata,headers=headers) 
+
+#把r的text的属性内容转换为json格式，并重新赋值给r
+r=json.loads(r.text) 
+
+#测试数据和返回数据做对比
+if testdata == r["data"]:
     print("测试通过")
 else:
     print("测试不通过")
